81154c009c7f53c196ed7591f30617582cd09eb1
GeonHyeock <gunheock@gmail.com>
2023-08-29 01:54:17 +0000

    Refactoring : rename

--- lightning-hydra-template/CoCa_pytorch/coca_pytorch/coca_dataset.py
+++ lightning-hydra-template/CoCa_pytorch/coca_pytorch/coca_dataset.py
@@ -8,20 +8,20 @@ from albumentations.pytorch import ToTensorV2
 
 
 class CocaDataset(Dataset):
-    def __init__(self, data_path, type, *trs):
-        self.data_path = data_path
+    def __init__(self, data_dir, data_type, *trs):
+        self.data_dir = data_dir
         self.transform = A.Compose(list(*trs), p=0.5)
 
-        csv_path = os.path.join(data_path, "train.csv")
+        csv_path = os.path.join(data_dir, "train.csv")
         data = pd.read_csv(csv_path)
-        self.data = data[data["type"] == type]
+        self.data = data[data["type"] == data_type].reset_index(drop=True)
 
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, idx):
         _, img_path, mos, comments, _ = self.data.iloc[idx]
-        img = cv2.imread(os.path.join(self.data_path, img_path))
+        img = cv2.imread(os.path.join(self.data_dir, img_path))
         img = self.transform(image=img)["image"]
 
         return {"img": img, "mos": mos, "text": comments}
