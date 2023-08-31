f6e585422931c2b77cdcddeab1f8b4d385af59c5
GeonHyeock <gunheock@gmail.com>
2023-08-31 11:57:52 +0900

    Feat : comment json

--- src/data_processing.py
+++ src/data_processing.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+import json
+from collections import defaultdict
 from sklearn.model_selection import train_test_split
 
 if __name__ == "__main__":
@@ -15,4 +17,11 @@ if __name__ == "__main__":
     data["type"] = pd.Series()
     data.loc[train_idx, "type"] = "train"
     data.loc[valid_idx, "type"] = "valid"
+
+    comments_dict = defaultdict(list)
+    for i in range(len(data)):
+        img_name, _, _, comments, _ = data.iloc[i]
+        comments_dict[img_name] += [comments]
+    with open("./data/comments_dict.json", "w") as f:
+        json.dump(comments_dict, f, indent=4)
     data.to_csv("data/train.csv", index=False)
