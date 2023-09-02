1ecff682056abc7202225d63de2e2d9c67633d3a
GeonHyeock <gunheock@gmail.com>
2023-09-02 15:44:12 +0900

    Feat : image id

--- lightning-hydra-template/Blip/dataset.py
+++ lightning-hydra-template/Blip/dataset.py
@@ -4,6 +4,7 @@ from PIL import Image
 import pandas as pd
 import os
 
+
 class ImageCaptioningDataset(Dataset):
     def __init__(self, data, processor, data_type):
         self.data_path = data
@@ -13,12 +14,16 @@ class ImageCaptioningDataset(Dataset):
         csv_path = os.path.join(data, "train.csv")
         data = pd.read_csv(csv_path)
         self.data = data[data["type"] == data_type].reset_index(drop=True)
+        if data_type == "valid":
+            self.data = self.data.iloc[
+                self.data.img_name.drop_duplicates().index
+            ].reset_index(drop=True)
 
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, idx):
-        img_name ,img_path,_,comments,_ = self.data.iloc[idx]
-        img = os.path.join(self.data_path,img_path)
+        _, img_path, _, comments, _, img_id = self.data.iloc[idx]
+        img = os.path.join(self.data_path, img_path)
 
-        return {"img" : img, "text" : comments, "img_name" : img_name}
+        return {"img": img, "text": comments, "img_id": img_id}
