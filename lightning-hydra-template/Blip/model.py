54fae4d560c1d4a697d3416f8f3f2d503369ac87
GeonHyeock <gunheock@gmail.com>
2023-09-02 15:43:57 +0900

    Feat : model freeze

--- lightning-hydra-template/Blip/model.py
+++ lightning-hydra-template/Blip/model.py
@@ -2,8 +2,29 @@ from transformers import AutoProcessor, BlipForConditionalGeneration
 
 class model:
     def __init__(self,pretrain):
+        """
+            text_decoder:
+                bert:
+                    embeddings
+                        embeddings
+                        encoder
+                cls:
+                    prediction:
+                        transform
+                        decoder
+        """
+        
         self.model = BlipForConditionalGeneration.from_pretrained(pretrain)
         self.processor = AutoProcessor.from_pretrained(pretrain)
+
+        for name, childs in self.model.named_children():
+            if name == "vision_model":
+                for param in childs.parameters():
+                    param.requires_grad = False
+
+            elif name == "text_decoder":
+                for param in childs.bert.parameters():
+                    param.requires_grad = False
         
     def return_model(self):
         return {"model" : self.model, "processor" : self.processor}
