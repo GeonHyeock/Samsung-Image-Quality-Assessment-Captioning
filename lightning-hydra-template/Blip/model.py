1677f4ae552692e43ccf29b74a1a59a8c614edee
GeonHyeock <gunheock@gmail.com>
2023-09-03 23:14:29 +0900

    Feat : nn module

--- lightning-hydra-template/Blip/model.py
+++ lightning-hydra-template/Blip/model.py
@@ -1,19 +1,21 @@
 from transformers import AutoProcessor, BlipForConditionalGeneration
+import torch.nn as nn
 
-class model:
-    def __init__(self,pretrain):
+
+class model(nn.Module):
+    def __init__(self, pretrain):
         """
-            text_decoder:
-                bert:
+        text_decoder:
+            bert:
+                embeddings
                     embeddings
-                        embeddings
-                        encoder
-                cls:
-                    prediction:
-                        transform
-                        decoder
+                    encoder
+            cls:
+                prediction:
+                    transform
+                    decoder
         """
-        
+        super(model, self).__init__()
         self.model = BlipForConditionalGeneration.from_pretrained(pretrain)
         self.processor = AutoProcessor.from_pretrained(pretrain)
 
@@ -25,10 +27,16 @@ class model:
             elif name == "text_decoder":
                 for param in childs.bert.parameters():
                     param.requires_grad = False
-        
-    def return_model(self):
-        return {"model" : self.model, "processor" : self.processor}
+
+    def forward(self, **x):
+        return self.model(**x)
 
 
+if __name__ == "__main__":
+    net = model("Salesforce/blip-image-captioning-base")
+    import torch
 
-
+    a = torch.load(
+        "/home/user/captioning/lightning-hydra-template/logs/train/runs/2023-09-03_12-45-16/checkpoints/epoch_000.ckpt"
+    )
+    pass
