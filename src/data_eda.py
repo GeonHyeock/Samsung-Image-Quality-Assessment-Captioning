828f0a066fa9cbbf9574a403a45361b995cc75c0
GeonHyeock <gunheock@gmail.com>
2023-09-05 23:23:51 +0900

    Feat : eda streamlit

---
+++ src/data_eda.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import streamlit as st
+import numpy as np
+import os
+import glob
+import random
+from PIL import Image
+
+
+def visual_img(d, img_name):
+    data = d[d.img_name == img_name]
+    img = Image.open(os.path.join("data", data.img_path.unique()[0]))
+    st.image(img)
+    st.write(f"mos : {data.mos.unique()[0]}")
+    for idx, value in enumerate(data.comments):
+        st.write(f"comments {idx} : {value}")
+
+
+def main():
+    st.title("Samsung Challenge Data EDA")
+
+    with st.sidebar:
+        csv = st.selectbox("시각화 할 csv", glob.glob("data/*.csv"))
+        if csv == "data/test.csv":
+            result = st.selectbox("정답 csv", glob.glob("./*.csv"))
+            raw_data = pd.merge(pd.read_csv(csv), pd.read_csv(result))
+        else:
+            raw_data = pd.read_csv(csv)
+        unique_img = raw_data.img_name.unique()
+
+        img_name = st.selectbox("이미지 이름 선택", unique_img)
+        img_idx = np.where(unique_img == img_name)
+
+        button = st.button("Random_choice", type="primary")
+        if button:
+            img_name = random.choice(unique_img)
+
+    visual_img(raw_data, img_name)
+
+
+if __name__ == "__main__":
+    main()
