24aef014e344f2d683fba3bf9017b32b8bcc08a3
GeonHyeock <gunheock@gmail.com>
2023-09-12 22:48:52 +0900

    Feat : lora

--- lightning-hydra-template/Blip/model.py
+++ lightning-hydra-template/Blip/model.py
@@ -1,4 +1,3 @@
-from transformers import AutoProcessor, BlipForConditionalGeneration
 from transformers import Blip2Processor, Blip2ForConditionalGeneration
 from peft import inject_adapter_in_model, LoraConfig
 import torch.nn as nn
@@ -6,35 +5,35 @@ import torch
 
 
 class model(nn.Module):
-    def __init__(self, pretrain):
+    def __init__(self, pretrain, train_module, lora_module):
         super(model, self).__init__()
         self.model = Blip2ForConditionalGeneration.from_pretrained(pretrain)
         self.processor = Blip2Processor.from_pretrained(pretrain)
 
         lora_target = [
             f"language_model.{name}"
-            for name, module in self.model.language_model.named_modules()
-            if isinstance(module, nn.Linear) or isinstance(module, nn.Conv1d)
+            for name, module in model.language_model.named_modules()
+            if ((isinstance(module, nn.Linear) or isinstance(module, nn.Conv1d)))
+            and any([m in name for m in lora_module])
+            and not any([m in name for m in train_module])
         ]
 
         lora_config = LoraConfig(
             lora_alpha=16,
-            lora_dropout=0.2,
+            lora_dropout=0.1,
             r=64,
             bias="lora_only",
             target_modules=lora_target,
         )
         self.model = inject_adapter_in_model(lora_config, self.model)
 
-        for p in self.model.qformer.parameters():
-            p.requires_grad = True
+        for name, param in self.model.named_parameters():
+            if any([m in name for m in train_module]):
+                param.requires_grad = True
 
-        for p in self.model.language_projection.parameters():
-            p.requires_grad = True
-
-        for i, v in self.model.named_parameters():
-            if v.requires_grad == True:
-                print(i)
+        for name, param in self.model.named_parameters():
+            if param.requires_grad == True:
+                print(name)
 
     def forward(self, **x):
         return self.model(**x)
