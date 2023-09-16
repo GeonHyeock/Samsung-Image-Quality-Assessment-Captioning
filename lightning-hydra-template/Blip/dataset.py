17db71f45498e62b07c0395f47cc51fef5a297ff
GeonHyeock <gunheock@gmail.com>
2023-09-16 11:04:46 +0900

    Add : text crop aug

--- lightning-hydra-template/Blip/dataset.py
+++ lightning-hydra-template/Blip/dataset.py
@@ -1,4 +1,6 @@
 from torch.utils.data import Dataset
+import nlpaug.augmenter.word as naw
+import random
 import pandas as pd
 import os
 
@@ -7,6 +9,7 @@ class ImageCaptioningDataset(Dataset):
     def __init__(self, data, data_type):
         self.data_path = data
         self.type = data_type
+        self.aug = naw.RandomWordAug(action="crop")
         if data_type == "test":
             csv_path = os.path.join(data, "test.csv")
             self.data = pd.read_csv(csv_path)
@@ -26,7 +29,10 @@ class ImageCaptioningDataset(Dataset):
     def __getitem__(self, idx):
         d = dict(self.data.iloc[idx])
         img = os.path.join(self.data_path, d["img_path"])
-        if self.type == "test":
+        if self.type in ["valid", "test"]:
             return {"img": img}
         else:
+            b = random.choice([True, False])
+            if b:
+                d["comments"] = self.aug.augment(d["comments"])[0]
             return {"img": img, "text": d["comments"], "img_id": d["image_id"]}
