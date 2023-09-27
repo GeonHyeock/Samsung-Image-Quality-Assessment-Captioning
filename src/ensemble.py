8b654df0220d87b48b65844aebe417af20b6d7e9
GeonHyeock <gunheock@gmail.com>
2023-09-27 17:44:17 +0900

    Feat : weight

--- src/ensemble.py
+++ src/ensemble.py
@@ -23,8 +23,8 @@ def make_weight(sentence, my_count):
         words = sentence.split()
         for i in range(len(words) - n + 1):
             ngram = " ".join(words[i : i + n])
-            weight += my_count.get(ngram, 2)
-    return len(words) / (np.log2(weight))
+            weight += my_count.get(ngram, 0)
+    return weight / len(words)
 
 
 def make_my_count():
@@ -36,9 +36,9 @@ def make_my_count():
     ngram_counts_3 = find_duplicate_ngrams(data.comments.unique(), 3)
     ngram_counts_4 = find_duplicate_ngrams(data.comments.unique(), 4)
 
-    my_count = {k: v for k, v in ngram_counts_2.items() if v > 1}
-    my_count.update({k: v for k, v in ngram_counts_3.items() if v > 1})
-    my_count.update({k: v for k, v in ngram_counts_4.items() if v > 1})
+    my_count = {k: np.log2(v / 2) for k, v in ngram_counts_2.items()}
+    my_count.update({k: np.log2(v / 3) for k, v in ngram_counts_3.items()})
+    my_count.update({k: np.log2(v / 4) for k, v in ngram_counts_4.items()})
 
     return my_count
 
@@ -47,10 +47,13 @@ if __name__ == "__main__":
     my_count = make_my_count()
     data = pd.concat([pd.read_csv(i) for i in glob("ensemble_csv/*.csv")])
 
+    for d in glob("ensemble_csv/*.csv"):
+        print(d)
+
     result = defaultdict(list)
     for img_name in tqdm(data.img_name.unique()):
         result["img_name"] += [img_name]
         comments = data[data.img_name == img_name].comments
-        weight = [make_weight(x, my_count) for x in comments]
-        result["comments"] += [max(zip(comments, weight), key=lambda x: x[1])[0]]
+        comments_weight = [(x, make_weight(x, my_count)) for x in comments]
+        result["comments"] += [max(comments_weight, key=lambda x: x[1])[0]]
     pd.DataFrame(result).to_csv("ensemble.csv", index=False)
