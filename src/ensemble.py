aa5ffdf1bc602e53316d28c10795044caec67c6a
GeonHyeock <gunheock@gmail.com>
2023-09-29 12:48:32 +0900

    Feat : voting count

--- src/ensemble.py
+++ src/ensemble.py
@@ -11,7 +11,7 @@ def main(score_dict):
         data.append(d)
     data = pd.concat(data)
 
-    img_names, comment, mos = [], [], []
+    img_names, comment, mos, i = [], [], [], 0
     for img_name in tqdm(data.img_name.unique()):
         d = data[data.img_name == img_name].loc[:, ["comments", "score"]]
 
@@ -23,18 +23,24 @@ def main(score_dict):
         comment += [max(result, key=result.get)]
         mos += [-1]
 
+        if len(result) < len(score_dict):
+            i += 1
+    print("ensemble N : ", i)
+
     return pd.DataFrame({"img_name": img_names, "comments": comment, "mos": mos})
 
 
 if __name__ == "__main__":
     score_dict = {
         "ensemble_csv/base_diffusion.csv": 1.276742618,
-        "ensemble_csv/base_diffusion_lora4.csv": 1.256808295,
-        "ensemble_csv/large_lora_4_diffusion.csv": 1.24555302,
+        # "ensemble_csv/base_diffusion_lora4.csv": 1.256808295,
+        # "ensemble_csv/large_lora_4_diffusion.csv": 1.24555302,
         # "ensemble_csv/base_diffusion_lora_8.csv": 1.234590897,
+        # "ensemble_csv/large_lora4.csv" : 1.220980674,
+        "ensemble_csv/base_fp16all.csv": 1.2036627085,
         # "ensemble_csv/base - diffusion + text train.csv": 1.183253507,
         # "ensemble_csv/large-diffusion-lora_4-train_text_decoder.csv": 1.153261951,
-        # "ensemble_csv/beamserch10.csv": 1.25,
+        # "ensemble_csv/beamserch10.csv": 1.25, - base random sample 0.9
         # "ensemble_csv/beamserch.csv": 1.25,
     }
 
