13cac467f7a08a9514e05c008f2d8567a3a9ebd8
GeonHyeock <gunheock@gmail.com>
2023-09-30 19:13:59 +0900

    Add : csv

--- src/ensemble.py
+++ src/ensemble.py
@@ -33,15 +33,14 @@ def main(score_dict):
 if __name__ == "__main__":
     score_dict = {
         "ensemble_csv/base_diffusion.csv": 1.276742618,
-        # "ensemble_csv/base_diffusion_lora4.csv": 1.256808295,
-        # "ensemble_csv/large_lora_4_diffusion.csv": 1.24555302,
-        # "ensemble_csv/base_diffusion_lora_8.csv": 1.234590897,
-        # "ensemble_csv/large_lora4.csv" : 1.220980674,
-        "ensemble_csv/base_fp16all.csv": 1.2036627085,
+        "ensemble_csv/large_beamserch.csv": 1.273379889,
+        "ensemble_csv/base_diffusion_lora4.csv": 1.256808295,
+        "ensemble_csv/large_lora4.csv": 1.247990337,
+        "ensemble_csv/large_lora_4_diffusion.csv": 1.24555302,
+        "ensemble_csv/base_diffusion_lora_8.csv": 1.234590897,
+        # "ensemble_csv/base_fp16all.csv": 1.2036627085,
         # "ensemble_csv/base - diffusion + text train.csv": 1.183253507,
         # "ensemble_csv/large-diffusion-lora_4-train_text_decoder.csv": 1.153261951,
-        # "ensemble_csv/beamserch10.csv": 1.25, - base random sample 0.9
-        # "ensemble_csv/beamserch.csv": 1.25,
     }
 
     df = main(score_dict)
