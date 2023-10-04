20dceed2787f38722d3d37985467c278a1f705aa
GeonHyeock <gunheock@gmail.com>
2023-10-04 12:51:57 +0900

    Add : sudo

--- requirement.sh
+++ requirement.sh
@@ -3,7 +3,7 @@ pip install --upgrade pip \
  && pip install -r ./requirements.txt \
  && pip install git+https://github.com/katsura-jp/pytorch-cosine-annealing-with-warmup
 
-apt-get update \
- && apt-get -y install libgl1-mesa-glx \
- && apt-get -y install openjdk-11-jdk \
- && apt-get install libglib2.0-0
+sudo apt-get update \
+ && sudo apt-get -y install openjdk-11-jdk \
+ && sudo apt-get -y install libglib2.0-0 \
+ && sudo apt-get -y install libgl1-mesa-glx
